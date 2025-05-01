64f0768107aeb693b636967c069757b34365a4fe
brightestsirius <katherina.shypovskaya@gmail.com>
2025-05-01 17:59:25 +0300

    lesson_04

---
+++ lesson_04/src/components/CRUD/CRUD.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import "./style.sass";
+import { handleEvent } from "./../../utils";
+
+const API = `https://jsonplaceholder.typicode.com/todos`;
+
+export default function CRUD() {
+  const [list, setList] = useState([]);
+
+  const getList = async () => {
+    try {
+      const request = await fetch(API),
+        response = await request.json();
+
+      setList(response.slice(0, 10));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getItemClassName = (item) => {
+    const classes = [`list__item`];
+    item.completed && classes.push(`list__item--completed`);
+
+    return classes.join(` `);
+  };
+
+  const handleItemDelete = async (id) => {
+    try {
+      await fetch(API + `/${id}`, { method: `DELETE` });
+      setList((prevState) => prevState.filter((item) => item.id !== id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleItemCompleted = async (item) => {
+    try {
+      const request = await fetch(API + `/${item.id}`, {
+          method: `PATCH`,
+          body: JSON.stringify({ completed: !item.completed }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }),
+        response = await request.json();
+
+      setList((prevState) =>
+        prevState.map((element) => {
+          if (element.id === response.id) element = response;
+          return element;
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  useEffect(() => {
+    console.log(`list`, list);
+  }, [list]);
+
+  return list.length ? (
+    <ul>
+      {list.map((item) => (
+        <li
+          key={item.id}
+          className={getItemClassName(item)}
+          onClick={() => handleItemCompleted(item)}
+        >
+          {item.title}{" "}
+          <button
+            onClick={(event) => handleEvent(event, handleItemDelete, [item.id])}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  ) : null;
+}
---
+++ lesson_04/src/mockData.js
@@ -0,0 +1,42 @@
+// structuredClone
+// 🔄🟢🟡🔴💌
+
+const ANIMALS = ["lion", "elephant", "giraffe", "zebra", "tiger", "panda"];
+
+const USER = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+};
+
+const TODOS = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "et porro tempora",
+    completed: true,
+  },
+];
+
+export { ANIMALS, USER, TODOS };
